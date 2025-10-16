674daf8db0716350d0ec59dbb514d7c79cbafb1a
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-16 21:15:42 +0530

    feat(App): add favicon and updated title and setup navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
+
+export const Navigation = () => {
+    return (
+        <div>Navigation</div>
+    )
+}
